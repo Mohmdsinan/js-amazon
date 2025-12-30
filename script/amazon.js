30dd24f501cf47902a601919adaf21fd632e8102
Mohmdsinan <sinankdty@gmail.com>
2025-12-30 11:09:24 +0530

    added cart button interactive

--- script/amazon.js
+++ script/amazon.js
@@ -48,13 +48,41 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button js-addToCart-btn button-primary" data-product-id= "${product.id}">
             Add to Cart
           </button>
         </div>
-  `  
+  `
 });
 
 
 document.querySelector(".products-grid")
   .innerHTML = productHTML;
+
+document.querySelectorAll('.js-addToCart-btn')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+
+      })
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId,
+          quantity: 1
+        })
+      }
+
+
+      console.log(cart);
+    })
+  })
